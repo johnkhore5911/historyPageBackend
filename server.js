349765757813cfef812f0c6303ea5a445b63b703
johnkhore5911 <johnkhore26@gmail.com>
2025-01-06 19:02:04 +0530

    added the mongodb url

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
-import express from "express"
-import mongoose from "mongoose"
-import bodyParser from "body-parser"
-import dotenv from "dotenv"
-import route from "./route/userRoute.js"
-
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import route from "./route/userRoute.js";
 
 const app = express();
 app.use(bodyParser.json());
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-const MONGOURL=process.env.MONGOURL;
+const MONGOURL = process.env.MONGOURL;
 
-mongoose
-.connect(MONGOURL)
-.then(()=>{
-    console.log('database connected successfully')
-    app.listen(PORT,()=>{
-        console.log('server is running on '+PORT);
+console.log("Mongourl: ",MONGOURL);
+
+const connect = () => {
+    mongoose.connect(MONGOURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Database connected successfully");
+
+        // Start the server after database connection is established
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("Database connection failed");
+        console.error(error);
+        process.exit(1); // Exit the process if DB connection fails
     });
-})
-.catch((error)=>console.log(error));
+};
 
-app.use("/api/user" , route);
+// Call the connect function to establish DB connection and start the server
+connect();
+
+app.use("/api/user", route);
