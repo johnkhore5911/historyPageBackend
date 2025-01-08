62208486398c0f1d819a99b45ab16bab682f7155
johnkhore5911 <johnkhore26@gmail.com>
2025-01-08 22:11:48 +0530

    dotenv fixed

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,7 +1,10 @@
 import express from "express"
 import { fetch , create , update, deleteUser} from "../controller/userController.js"
-import upload from "../../backend practice/middlewares/multer/multer.js"
-import uploadMultiple from "../../backend practice/middlewares/upload/uploadMultiple.js"
+// import upload from "../../backend practice/middlewares/multer/multer.js"
+import upload from '../middlewares/multer/multer.js'
+// import uploadMultiple from "../../backend practice/middlewares/upload/uploadMultiple.js"
+
+import uploadMultiple from '../middlewares/upload/uploadMultiple.js'
 //const upload = require ("../../middlewares/multer/multer.js");
 //const uploadMultiple = require("../../middlewares/upload/uploadMultiple.js");
 
